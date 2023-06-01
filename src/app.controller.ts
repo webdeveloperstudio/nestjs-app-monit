087bb461cf9bb01736129059dda6c7cbee8d4006
MerabC <merabchik83@gmail.com>
2023-06-02 02:11:00 +0400

    Setup MicroService server on TCP protocol

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, EventPattern, NatsContext, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('app.shutdown')
+  appShutdown(@Payload() message: any, @Ctx() context: NatsContext): void {
+    console.log('app.shutdown');
+    console.log(message);
+    console.log(context);
+    this.appService.appDisabled(message);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+const MSM_HOST: string = process.env.host || 'localhost';
+const MSM_PORT: number = parseInt(process.env.host) || 3001;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: MSM_HOST,
+      port: MSM_PORT
+    }
+  });
+  await app.listen();
 }
-bootstrap();
+bootstrap().then(() => console.log(`Microservice is listening on: ${MSM_HOST}:${MSM_PORT}`));
